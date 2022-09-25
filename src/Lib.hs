9fac30a0d12a5b119054259bcf32b4ef1d97e569
Adowrath <cyrill.brunner@hispeed.ch>
2022-09-25 14:53:33 +0200

    Add Shakespearean Templating

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,24 +8,20 @@ import Relude hiding (Product)
 
 import qualified Data.Map.Lazy as M
 import Data.Text (isInfixOf)
-import Network.HTTP.Media ((//))
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Product
 import RestApi
 import Servant
 import Servant.API.Generic
-
-data RawHTML
-
-instance Accept RawHTML where
-  contentType _ = "text" // "html"
-instance MimeRender RawHTML Text where
-  mimeRender _ text = encodeUtf8 text
+import Servant.HTML.Blaze
+import Text.Blaze
+import Text.Hamlet
+import Text.Julius
 
 type API =
   "api" :> ToServantApi (RestAPI "product" '[JSON] Product)
-  :<|> "search" :> QueryParam "searchTerm" Text :> Get '[RawHTML] Text
+  :<|> "search" :> QueryParam "searchTerm" Text :> Get '[HTML] Html
 
 startApp :: IORef (Int, Map Int Product)
          -> IO ()
@@ -44,90 +40,92 @@ server ref = ioRefRestAPI ref :<|> searchApi ref
 
 searchApi :: IORef (Int, Map Int Product)
           -> Maybe Text
-          -> Handler Text
+          -> Handler Html
 searchApi ref filterTerm = readIORef ref <&> (M.elems . snd) >>= \products -> do
   let infixFilter :: Text -> Product -> Bool
       term `infixFilter` p = term `isInfixOf` name p || term `isInfixOf` description p
 
       searchFilter :: [Product] -> [Product]
-      searchFilter = maybe id (filter . infixFilter) filterTerm
+      searchFilter = maybe id (filter . infixFilter) $ mfilter (/= "") filterTerm
 
-      productToLi :: Product -> Text
-      productToLi (Product {..}) = mconcat [ "<li> (", show productId, ") - ", name, ": ", description ]
+      filteredProducts :: [Product]
+      filteredProducts = searchFilter products
 
-      productLines :: [Text]
-      productLines = map productToLi $ searchFilter products
+      script :: Text
+      script = toStrict $ renderJavascriptUrl undefined [julius|
+        function addItem(productId, name, description) {
+          const list = document.getElementById('list');
+          if(list instanceof HTMLDivElement) {
+            const parent = list.parentElement;
+            list.remove();
+            parent.innerHTML += `<ul><li> (${productId}) - ${name}: ${description}</ul>`;
+          } else {
+            list.innerHTML += `<li> (${productId}) - ${name}: ${description}`;
+          }
+        }
 
-      productBlock :: [Text]
-      productBlock =
-        if null productLines
-        then [ "<div id=\"list\"><strong>No products found.</strong></div>" ]
-        else "<ul id=\"list\">" : productLines <> [ "</ul>" ]
+        async function createNewItem(event) {
+          event.preventDefault();
+          const nameElem = document.getElementById('nameInput');
+          const descElem = document.getElementById('descInput');
 
-  pure $ unlines
-    (
-      [ "<!DOCTYPE html>"
-      , "<html><head><title>Search Interface</title></head><body>"
-      , "<script>"
-      , "function addItem(productId, name, description) {"
-      , "  const list = document.getElementById('list');"
-      , "  if(list instanceof HTMLDivElement) {"
-      , "    const parent = list.parentElement;"
-      , "    list.remove();"
-      , "    parent.innerHTML += `<ul><li> (${productId}) - ${name}: ${description}</ul>`;"
-      , "  } else {"
-      , "    list.innerHTML += `<li> (${productId}) - ${name}: ${description}`;"
-      , "  }"
-      , "}"
+          const name = nameElem.value;
+          const description = descElem.value;
 
-      , "async function createNewItem(event) {"
-      , "  event.preventDefault();"
-      , "  const nameElem = document.getElementById('nameInput');"
-      , "  const descElem = document.getElementById('descInput');"
+          const res = await fetch('http://localhost:8080/api/product', {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({productId: -1, name, description }),
+            method: 'POST'
+          });
 
-      , "  const name = nameElem.value;"
-      , "  const description = descElem.value;"
+          if(res.ok) {
+            const {productId, name, description} = await res.json();
+            addItem(productId, name, description);
+          } else { alert('An error has occurred: ' + res.statusText); }
 
-      , "  const res = await fetch('http://localhost:8080/api/product', {"
-      , "    headers: {"
-      , "      'accept': 'application/json',"
-      , "      'Content-Type': 'application/json'"
-      , "    },"
-      , "    body: JSON.stringify({productId: -1, name, description }),"
-      , "    method: 'POST'"
-      , "  });"
+          nameElem.value = '';
+          descElem.value = '';
+          nameElem.focus();
+        }
 
-      , "  if(res.ok) {"
-      , "    const {productId, name, description} = await res.json();"
-      , "    addItem(productId, name, description);"
-      , "  } else { alert('An error has occurred: ' + res.statusText); }"
+        document.addEventListener('DOMContentLoaded', () => {
+          document.getElementById('addProduct').addEventListener('submit', createNewItem);
+        });
+      |]
 
-      , "  nameElem.value = '';"
-      , "  descElem.value = '';"
-      , "}"
-      , "document.addEventListener('DOMContentLoaded', () => {"
-      , "  document.getElementById('addProduct').addEventListener('submit', createNewItem);"
-      , "});"
-      , "</script>"
-      , "<h1>Add new product</h1>"
-      , "<form method=\"POST\" id=\"addProduct\" action=\"\">"
-      , "    <label for=\"nameInput\">Product name:</label>"
-      , "    <input name=\"nameInput\" id=\"nameInput\" type=\"text\">"
-      , "    <br>"
-      , "    <label for=\"descInput\">Product description:</label>"
-      , "    <input name=\"descInput\" id=\"descInput\" type=\"text\">"
-      , "    <br>"
-      , "    <button type=\"submit\">Add Product</button>"
-      , "</form>"
-      , "<h1>Search for product</h1>"
-      , "<form method=\"GET\" action=\"\">"
-      , "    <label for=\"searchTerm\">Search Term:</label>"
-      , "    <input name=\"searchTerm\" id=\"searchTerm\" type=\"text\">"
-      , "    <button type=\"submit\">Search</button>"
-      , "</form>"
-      , "<h1>Product list</h1>"
-      ] <> productBlock <>
-      [ "</body></html>"
-      ]
-    )
+  pure [shamlet|
+    $doctype 5
+    <html>
+        <head>
+            <title>Search Interface
+            <script>
+              #{preEscapedToMarkup script}
+        <body>
+            <h1>Add a new Product
+            <form method="POST" #addProduct action="">
+                <label for="nameInput">Product name:
+                <input name="nameInput" #nameInput type="text">
+                <br>
+                <label for="descInput">Product description:
+                <input name="descInput" #descInput type="text">
+                <br>
+                <button type="submit">Add Product
 
+            <h1>Search for Products
+            <form method="GET" action="">
+                <label for="searchTerm">Search Term:
+                <input name="searchTerm" #searchTerm type="text" value="#{fromMaybe "" filterTerm}">
+                <button type="submit">Search
+
+            <h1>Product List
+            $if null products
+                <div #list>
+                    <strong>No products found.
+            $else
+                <ul #list>
+                    $forall Product {..} <- filteredProducts
+                        <li>(#{productId}) - #{name}: #{description}
+  |]
